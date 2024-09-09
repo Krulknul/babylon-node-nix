5a530284aedf07b13419a34350d991a4499f773c
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-09 21:58:19 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,22 @@
 {
-  description = "template for developing/testing nixos modules";
+  description = "A NixOS module for the RadixDLT Babylon node software";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }:
+  let
+      forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in
     {
-      nixosModules.babylon_node = import ./babylon-service.nix;
+        packages = forAllSystems (pkgs: {
+            default = pkgs.callPackage ./babylon-node.nix {inherit nixpkgs};
+        });
+        nixosModules.babylon_node = import ./babylon-service.nix;
     };
 }
