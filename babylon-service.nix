276062f38a602896ff52fa5f5f9541d241045a7f
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-10 02:30:56 +0200

    fix

--- babylon-service.nix
+++ babylon-service.nix
@@ -47,7 +47,7 @@ in
       serviceConfig = {
         User = cfg.config.run_with.user;
         Group = cfg.config.run_with.group;
-        ExecStart = "${babylon-node}/bin/babylonnode -config /etc/radixdlt/babylon_node.config";
+        ExecStart = "${babylon-node}/bin/babylon_node -config /etc/radixdlt/babylon_node.config";
         Restart = "always";
         WorkingDirectory = cfg.config.run_with.working_directory;
         EnvironmentFile = cfg.config.run_with.environment_file;
