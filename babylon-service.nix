7d5ca486ec9d29c8efccb3bc510c5838de0a3c2d
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-10 03:10:01 +0200

    asd

--- babylon-service.nix
+++ babylon-service.nix
@@ -9,6 +9,7 @@
 with lib;
 let
   babylon-node = self.packages.aarch64-darwin.default;
+  print = builtins.trace "babylon-node: ${babylon-node}" "";
   options = import ./options.nix { inherit lib; };
   cfg = config.services.babylon_node;
   boolToString = b: if b then "true" else "false";
