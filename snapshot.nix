9959d7c5c20687b5ad944298287e8f59eb25eea3
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-12 13:06:09 +0200

    subcommands

--- snapshot.nix
+++ snapshot.nix
@@ -11,62 +11,78 @@ pkgs.stdenv.mkDerivation {
 
   installPhase =
   let script = ''
-    #!${pkgs.bash}/bin/bash
+ #!${pkgs.bash}/bin/bash
 
-    echo "Running this script will:
-  - Stop your Babylon node.
-  - Download the latest snapshot from snapshots.radix.live.
-  - Wipe the ledger database directory as set in your NixOS configuration.
-  - Extract the snapshot to the database directory.
-  - Start your Babylon node.
-
-Are you sure you wish to continue? [y/N]
-    "
-
-    while true; do
-        read -p "Do you wish to continue? (y/N) " yn
-        case \$yn in
-            [Yy]*) break;;
-            [Nn]* ) echo "Aborting"; exit;;
-            * ) echo "Aborting."; exit;;
-        esac
-    done
-
-    echo "Stopping the Radix node.."
+function stop_node() {
+    echo -e "\e[32mStopping the Radix node..\e[0m"
     systemctl stop babylon-node
+}
 
-
-    CURRENT_DATE=\$(date +"%Y-%m-%d")
-
-    mkdir ${dbDir}/download
-    echo "Downloading the latest snapshot..."
+function download_snapshot() {
+    mkdir -p ${dbDir}/download
+    echo -e "\e[34mDownloading the latest snapshot...\e[0m"
     max_retries=5
     attempt_num=1
     while [ \$attempt_num -le \$max_retries ]
     do
-      ${pkgs.aria2}/bin/aria2c -x3 -s16 -k4M --piece-length=4M --disk-cache=256M --lowest-speed-limit=250k ftp://snapshots.radix.live/\$CURRENT_DATE/RADIXDB-INDEX.tar.zst.metalink -d ${dbDir}/download && break
-      echo "Download failed, attempt \$attempt_num of \$max_retries..."
+      echo -n "\e[34mAttempt \$attempt_num of \$max_retries: \e[0m"
+      if ${pkgs.aria2}/bin/aria2c -x3 -s16 -k4M --piece-length=4M --disk-cache=256M --lowest-speed-limit=250k ftp://snapshots.radix.live/\$(date +"%Y-%m-%d")/RADIXDB-INDEX.tar.zst.metalink -d ${dbDir}/download; then
+        echo -e "\e[32mDownload successful!\e[0m"
+        break
+      else
+        echo -e "\e[31mDownload failed.\e[0m"
+      fi
       ((attempt_num++))
     done
 
     if [ \$attempt_num -gt \$max_retries ]; then
-      echo "Failed to download after \$max_retries attempts, aborting."
+      echo -e "\e[31mFailed to download after \$max_retries attempts, aborting.\e[0m"
       exit 1
     fi
+}
 
+function wipe_ledger() {
     shopt -s extglob
-    echo "Wiping the ledger database directory..."
-    rm -rf  ${dbDir}/!(download)
+    echo -e "\e[33mWiping the ledger database directory...\e[0m"
+    rm -rf ${dbDir}/!(download)
+}
 
-    echo "Extracting the snapshot..."
-    ${pkgs.zstd}/bin/zstd -d ${dbDir}/download/RADIXDB-INDEX.tar.zst --stdout | ${pkgs.gnutar}/bin/tar xvf - -C ${dbDir}
+function extract_snapshot() {
+    echo -e "\e[34mExtracting the snapshot...\e[0m"
+    ${pkgs.zstd}/bin/zstd -d ${dbDir}/download/RADIXDB-INDEX.tar.zst --stdout | tar -xvf - -C ${dbDir} --checkpoint=10 --checkpoint-action=exec='echo -n "."'
+    echo -e "\n\e[32mExtraction complete.\e[0m"
+}
 
-    echo "Cleaning up..."
+function cleanup() {
+    echo -e "\e[33mCleaning up...\e[0m"
     rm -rf ${dbDir}/download
     rm -rf ${dbDir}/address-book
+}
 
-    echo "Starting the Radix node.."
+function start_node() {
+    echo -e "\e[32mStarting the Radix node..\e[0m"
     systemctl start babylon-node
+}
+
+case "\$1" in
+    download)
+        download_snapshot
+        ;;
+    extract)
+        extract_snapshot
+        ;;
+    all)
+        stop_node
+        download_snapshot
+        wipe_ledger
+        extract_snapshot
+        cleanup
+        start_node
+        ;;
+    *)
+        echo "Usage: \$0 {download|extract|all}"
+        ;;
+esac
   '';
   in
   ''
