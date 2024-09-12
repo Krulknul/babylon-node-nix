13efefe735e164cf4a9eb01410eaa6f7dbc5c6f9
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-12 02:44:43 +0200

    moar echos

--- snapshot.nix
+++ snapshot.nix
@@ -30,26 +30,28 @@ pkgs.stdenv.mkDerivation {
         esac
     done
 
-    # Stop the Radix node
+    echo "Stopping the Radix node.."
     systemctl stop babylon-node
 
-    # Wipe the ledger database directory
+    echo "Wiping the ledger database directory..."
     rm -rf ${dbDir}/**
     mkdir ${dbDir}/download
 
     CURRENT_DATE=$(date +"%Y-%m-%d")
 
     # Download the latest snapshot
+    echo "Downloading the latest snapshot..."
     ${pkgs.aria2}/bin/aria2c -x3 -s16 -k4M --piece-length=4M --disk-cache=256M --lowest-speed-limit=250k ftp://snapshots.radix.live/\$CURRENT_DATE/RADIXDB-INDEX.tar.zst.metalink -d ${dbDir}/download
 
-    # Extract the snapshot
+    echo "Extracting the snapshot..."
     ${pkgs.zstd}/bin/zstd -d ./dir/download/RADIXDB-INDEX.tar.zst --stdout | tar xvf - -C ${dbDir}
 
-    # Clean up
+    echo "Cleaning up..."
     rm -rf ${dbDir}/download
     rm -rf ${dbDir}/address-book
 
     # Start the Radix node
+    echo "Starting the Radix node.."
     systemctl start babylon-node
   '';
   in
