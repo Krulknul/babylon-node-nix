2d1136d4cc911f1785dc0deda4170b5564ddf2a8
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-09-14 13:47:10 +0200

    fix

--- babylon-service.nix
+++ babylon-service.nix
@@ -59,7 +59,8 @@ in
         serviceConfig = {
           User = cfg.config.run_with.user;
           Group = cfg.config.run_with.group;
-          ExecStart = "/bin/sh -c 'export RADIX_NODE_KEYSTORE_PASSWORD=\$(cat ${cfg.config.run_with.keystore_password_file}) ${babylon-node}/bin/babylon-node -config /etc/radixdlt/babylon-node.config'";
+          ExecStartPre="/bin/sh -c 'export RADIX_NODE_KEYSTORE_PASSWORD=\$(cat ${cfg.config.run_with.keystore_password_file})'";
+          ExecStart = "${babylon-node}/bin/babylon-node -config /etc/radixdlt/babylon-node.config";
           Restart = "always";
           WorkingDirectory = cfg.config.run_with.working_directory;
           LimitNOFILE = 65536;
